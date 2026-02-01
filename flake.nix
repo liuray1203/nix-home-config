9c7534d155fd15cceb8ce285ec582a641348f911
liuray <you@example.com>
2026-02-01 08:26:22 +0800

    init nix + home-manager config

---
+++ flake.nix
@@ -0,0 +1,33 @@
+ {
+  nixConfig = {
+   experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  description = "liuray Home Manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+
+    in {
+      homeConfigurations."liuray" =
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [
+            ./home.nix
+          ];
+        };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,78 @@
+{ config, pkgs, ... }:
+
+{
+home.username = "liuray";
+home.homeDirectory = "/home/liuray";
+home.stateVersion = "24.05";
+
+
+# ----------------------------
+# 基礎系統工具（CLI / Dev / Laptop CLI）
+# ----------------------------
+home.packages = with pkgs; [
+# CLI 基礎
+git
+curl
+wget
+unzip
+zip
+rsync
+tree
+htop
+btop
+fastfetch
+ripgrep
+fd
+
+
+# 開發 / 腳本
+python3
+python3Packages.pip
+bash
+zsh
+shellcheck
+shfmt
+
+
+# Node / 編輯器
+nodejs
+neovim
+
+
+# ThinkPad CLI 工具（不啟動 systemd 服務）
+tlp # 使用前請用 pacman 安裝並啟用服務
+brightnessctl
+];
+
+
+# ----------------------------
+# Shell 設定
+# ----------------------------
+programs.zsh = {
+enable = true;
+enableCompletion = true;
+autosuggestion.enable = true;
+syntaxHighlighting.enable = true;
+};
+
+
+programs.starship = {
+enable = true;
+};
+
+
+# ----------------------------
+# Git 設定
+# ----------------------------
+programs.git = {
+enable = true;
+userName = "liuray";
+userEmail = "you@example.com"; # 之後再改
+};
+
+
+# ----------------------------
+# Home Manager 本體
+# ----------------------------
+programs.home-manager.enable = true;
+}
