75f420ed61e1a319a4b6dac95b107425a7f25179
liuray <you@example.com>
2026-03-01 08:26:33 +0800

    update config

--- home.nix
+++ home.nix
@@ -59,21 +59,21 @@ enable = true;
 settings = {
     add_newline = false;
     format = ''
-[░▒▓](#a3aed2)\
-[  ](bg:#a3aed2 fg:#090c0c)\
-[](bg:#769ff0 fg:#a3aed2)\
-$directory\
-[](fg:#769ff0 bg:#394260)\
-$git_branch\
-$git_status\
-[](fg:#394260 bg:#212736)\
-$nodejs\
-$rust\
-$golang\
-$php\
-[](fg:#212736 bg:#1d2230)\
-$time\
-[ ](fg:#1d2230)\
+[░▒▓](#a3aed2)
+[  ](bg:#a3aed2 fg:#090c0c)
+[](bg:#769ff0 fg:#a3aed2)
+$directory
+[](fg:#769ff0 bg:#394260)
+$git_branch
+$git_status
+[](fg:#394260 bg:#212736)
+$nodejs
+$rust
+$golang
+$php
+[](fg:#212736 bg:#1d2230)
+$time
+[ ](fg:#1d2230)
 \n$character
 '';
 
